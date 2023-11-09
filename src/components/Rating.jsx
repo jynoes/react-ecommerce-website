e3e5fdce47b05420d517f9e85ed6b91307ad84a3
jynoes <89066947+jynoes@users.noreply.github.com>
2023-11-09 15:31:47 +0800

    3rd initial commit
    added category sections

---
+++ src/components/Rating.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Rating = () => {
+  return (
+    <span className='ratting'>
+        <i class="icofont-ui-rating"></i>
+        <i class="icofont-ui-rating"></i>
+        <i class="icofont-ui-rating"></i>
+        <i class="icofont-ui-rating"></i>
+        <i class="icofont-ui-rating"></i>
+    </span>
+  )
+}
+
+export default Rating
