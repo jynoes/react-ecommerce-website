c341752e84e76c25fc501210e1ce3d39d47d6837
jynoes <89066947+jynoes@users.noreply.github.com>
2023-11-09 12:46:03 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Outlet } from 'react-router-dom'
+import NavItems from './components/NavItems'
+
+function App() {
+  return (
+    <>
+      <NavItems/>
+      <Outlet/>
+    </>
+  )
+}
+
+export default App
